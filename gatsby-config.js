e845d7b302572aefec3e89ce0faaf22c47cf6fcb
Harold <devwebecomme@gmail.com>
2020-07-06 00:37:11 -0400

    refactor: change method to work with fonts - typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: "src/utils/typography.js"
+        pathToConfigModule: "src/utils/typography"
       }
     },
     `gatsby-plugin-sass`,
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,38 +1,7 @@
 import Typography from "typography";
-//import theme from "typography-theme-lawton";
-//const typography = new Typography(theme);
-/**
- * We can even override any of the theme's styles
- */
+import kirkhamTheme from "typography-theme-kirkham";
 
-/*theme.overrideThemeStyles = () => ({
-    a: {
-      color: "black",
-      textDecoration: "none",
-      borderBottom: "2px solid gold"
-    },
-  
-    "a:hover": {
-      color: "black",
-      textDecoration: "none",
-      borderBottom: "2px solid gold"
-    }
-  });*/
-  
-  // const typography = new Typography({
-  //   baseFontSize: "18px",
-  //   baseLineHeight: 1.45,
-  //   headerFontFamily: ["MuseoModerno", "sans-serif"],
-  //   bodyFontFamily: ["Roboto", "sans-serif"]
-  // });
-
-
-const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.45,
-  headerFontFamily: ["Playfair Display", "serif"],
-  bodyFontFamily: ["Roboto", "sans-serif"]
-});
+const typography = new Typography(kirkhamTheme);
 
 // Insert styles directly into the <head>
 typography.injectStyles();
